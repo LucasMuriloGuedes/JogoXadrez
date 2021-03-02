fa50ff0c46ea84904ad5a9fc17371bf605918016
LUCAS MURILO GUEDES OLIVEIRA <lucasmurilo.guedes@gmail.com>
2021-03-01 21:38:52 -0400

    Enviando atividades do curso de java para o github

---
+++ Jogo Xadrez/src/boardgame/Piece.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boardgame;
+
+/**
+ *
+ * @author Lucas Murilo
+ */
+public class Piece {
+    
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+       
+    
+}
